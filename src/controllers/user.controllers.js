7ac74a7164c9750da968f03c496c0ede48b77bc5
SHISHIR1507 <singhshishir4727@gmail.com>
2025-07-12 23:17:10 +0530

    Add watch history API with nested video and owner lookup using Aggregation Pipelines

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -295,4 +295,123 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
     res.status(200).json(new ApiResponse(200, user, "Cover image updated successfully"));
 });
 
-export { registerUser, loginUser, refreshAccessToken ,logoutUser , changeCurrentPassword,getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage }; 
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+
+    const {username}=req.params
+    if(!username?.trim()) {
+        throw new ApiError(400, "Username is required");
+    }
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {   
+                subscriberCount: 
+                {
+                     $size: "$subscribers" 
+            },
+                channelsSubscribedToCount: 
+                {
+                     $size: "$subscribedTo" 
+                    },
+            
+            
+            isSubscribed: {
+                $cond: {
+                    if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                    then: true,
+                    else: false
+                }
+            }
+            }
+        },
+        {
+            $project: {
+                fullname: 1,
+                username: 1,
+                avatar: 1,
+                coverImage: 1,
+                subscriberCount: 1,
+                email: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1
+            
+            }
+        }
+    ])
+    if(!channel.length) {
+        throw new ApiError(404, "Channel not found");
+    }
+    return res.status(200).json(new ApiResponse(200, channel[0], "User channel profile fetched successfully"));
+
+
+})
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user=await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistoryVideos",
+            pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "ownerDetails",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                            $addFields: {
+                                owner: {
+                                    $first: "$ownerDetails"
+                                }
+                            }
+                        }
+                    
+                ]
+            }
+        },
+    ])
+
+    return res.status(200).json(new ApiResponse(200, user[0]?.watchHistoryVideos, "Watch history fetched successfully"));
+
+})
+export { registerUser, loginUser, refreshAccessToken ,logoutUser , changeCurrentPassword,getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory };
